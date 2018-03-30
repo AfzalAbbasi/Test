f985a99af474d394be9829c76a12bfc46cdc7a77
Afzal Abbasi <35796329+AfzalAbbasi@users.noreply.github.com>
2018-03-30 11:16:43 +0500

    Update main.go

--- main.go
+++ main.go
@@ -1,165 +1,135 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"io/ioutil"
 	"net/http"
-	"time"
-
-	_ "github.com/go-sql-driver/mysql"
+	"os"
+	"strconv"
 )
 
-var record []string
-
-type State struct {
-	DisplayString string `json:"displayString"`
+type User struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
 }
-type DeviceData struct {
-	Device   string `json:"device"`
-	Online   bool   `json:"online"`
-	Firmware string `json:"firmware"`
-	State    `json:"sp_regstate"`
-	Number   `json:"number"`
-}
-
 type Message struct {
-	DisplayMessage string `json:"message"`
+	message string `json:"message"`
+}
+type Delete struct {
+	delete string `json:"delete"`
+}
+type Data struct {
+	data string `json:"data"`
+	User
 }
 
-type Number struct {
-	SerialNumber string           `json:"serialNumber" db:"serialnumber"`
-	AlarmType    string           `json:"alarmType" db:"alarmtype"`
-	CreateDate   string           `json:"createTime"`
-	Primary      PrimaryContact   `json:"primaryContact" db:"primary"`
-	Secondary    SecondaryContact `json:"secondaryContact" db"secondary"`
-}
-
-type PrimaryContact struct {
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Email     string `json:"email"`
-	Phone     string `json:"phone"`
-	Mobile    string `json:"mobile"`
-}
-type SecondaryContact struct {
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Email     string `json:"email"`
-	Phone     string `json:"phone"`
-	Mobile    string `json:"mobile"`
-}
-
-const (
-	host     = ""
-	port     = 3306
-	user     = "root"
-	password = ""
-	dbname   = "mysql"
+var (
+	counter = 0
+	name    string
+	last    string
+	e       = echo.New()
 )
-
-var db *sql.DB
 
 func main() {
-	http.HandleFunc("/dataupload", postdata)
-	http.HandleFunc("/getdata", getdata)
-	http.ListenAndServe(":8080", nil)
+	// Echo instance
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.Static("/", "public")
+	e.POST("/users", createUser)
+	e.GET("/users/:id", getUser)
+	e.PUT("/update/:id", updateUser)
+	e.DELETE("/delete/:id", deleteUser)
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
 }
-func postdata(w http.ResponseWriter, req *http.Request) {
-	var data Number
-	res := req.Body
-	fmt.Println(res)
-	rep, err := ioutil.ReadAll(res)
-	if err != nil {
-		fmt.Println(err)
-	}
-	json.Unmarshal(rep, &data)
-	db, err := sql.Open("mysql", "")
-	if err != nil {
-		fmt.Println(err)
-	}
-	var id = 0
-	_, err = db.Query("INSERT INTO Test(id,serialnumber,alarmtype,createtime,Pfirstname,Plastname,Pemail,Pphone,Pmobile,Sfirstname,Slastname,Semail,Sphone,Smobile) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)", id, data.SerialNumber, data.AlarmType, time.Now().UTC(), data.Primary.FirstName, data.Primary.LastName, data.Primary.Email, data.Primary.Phone, data.Primary.Mobile, data.Secondary.FirstName, data.Secondary.LastName, data.Secondary.Email, data.Secondary.Phone, data.Secondary.Mobile)
-	if err != nil {
-		BadResponse(w, "Data are Not Uploaded")
-	} else {
-		CreateSuccessResponse(w, "Data are Uploded")
-	}
-	defer db.Close()
 
+// Handler
+
+func createUser(c echo.Context) error {
+
+	// Read form fields
+	name = c.FormValue("first")
+	last = c.FormValue("last")
+	counter++
+	data := &User{counter, name, last}
+
+	b, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	if err != nil {
+
+		return err
+	}
+	fileName := fmt.Sprintf("./%d.json", counter)
+	f, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+
+		return err
+	}
+	f.Write(b)
+	f.Close()
+	return CreateSuccessResponse(&c, 200, "successful", "data", b)
+	//return c.JSON(http.StatusOK, User{counter, name, last})
 }
-func getdata(w http.ResponseWriter, req *http.Request) {
-	queryValues := req.URL.Query()
-	id := queryValues.Get("id")
-	uptime := queryValues.Get("up")
-	totime := queryValues.Get("to")
-	db, err := sql.Open("mysql", "")
-	if err != nil {
-		fmt.Println(err)
-	}
-	data, err := db.Query("select * from Devices d, Test t where d.device = t.serialnumber and d.device = ? and t.createtime BETWEEN ? AND ?", id, uptime, totime)
-	if err != nil {
-		fmt.Print(err)
-	}
 
-	emp := DeviceData{}
-	res := []DeviceData{}
-	for data.Next() {
-		var id int
-		var online bool
-		var device, firmware, createtime, serialnumber, spRegStaee, alarmtype, Pfirstname, Plastname, Pemail, Pphone, Pmobile, Sfirstname, Slastname, Semail, Sphone, Smobile string
-		err = data.Scan(&id, &device, &online, &firmware, &spRegStaee, &id, &serialnumber, &alarmtype, &createtime, &Pfirstname, &Plastname, &Pemail, &Pphone, &Pmobile, &Sfirstname, &Slastname, &Semail, &Sphone, &Smobile)
-		if err != nil {
-			fmt.Println(err)
-		}
-		emp.Device = device
-		emp.Online = online
-		emp.Firmware = firmware
-		emp.DisplayString = spRegStaee
-		emp.SerialNumber = serialnumber
-		emp.AlarmType = alarmtype
-		emp.CreateDate = createtime
-		emp.Primary.FirstName = Pfirstname
-		emp.Primary.LastName = Plastname
-		emp.Primary.Email = Pemail
-		emp.Primary.Phone = Pphone
-		emp.Primary.Mobile = Pmobile
-		emp.Secondary.FirstName = Sfirstname
-		emp.Secondary.LastName = Slastname
-		emp.Secondary.Email = Semail
-		emp.Secondary.Phone = Sphone
-		emp.Secondary.Mobile = Smobile
-		res = append(res, emp)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Print(res)
-	messagee, err := json.Marshal(res)
+func getUser(c echo.Context) error {
+	count, _ := strconv.Atoi(c.Param("id"))
+	fileName := fmt.Sprintf("./%d.json", count)
+	raw, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
-	w.Write(messagee)
+	var data User
+	json.Unmarshal(raw, &data)
+	fmt.Println(data)
+
+	return c.JSON(http.StatusOK, &data)
 
 }
-func CreateSuccessResponse(w http.ResponseWriter, message string) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	status := Message{message}
-	messagee, err := json.Marshal(status)
-	if err != nil {
-		fmt.Println(err)
-	}
 
-	w.Write(messagee)
-}
-func BadResponse(w http.ResponseWriter, message string) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusNotFound)
-	status := Message{message}
-	messagee, err := json.Marshal(status)
+func deleteUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	fileName := fmt.Sprintf("./%d.json", id)
+	raw, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err.Error())
+		os.Exit(1)
+		fmt.Print("error")
 	}
-	w.Write(messagee)
+	var data User
+	json.Unmarshal(raw, &data)
+	os.Remove(fileName)
+
+	return c.JSON(http.StatusOK, &Delete{"Deleted"})
+}
+func updateUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	fileName := fmt.Sprintf("./%d.json", id)
+	var data = User{counter, "danish", "khan"}
+	b, err := json.Marshal(data)
+	f, err := os.OpenFile(fileName, os.O_WRONLY, 0666)
+	if err != nil {
+
+		return err
+	}
+	f.Write(b)
+	f.Close()
+	return c.JSON(http.StatusOK, &Message{"User are Updated"})
+}
+func CreateSuccessResponse(c *echo.Context, requestCode int, message string, subMessage string, data []byte) error {
+
+	localC := *c
+	response := fmt.Sprintf("{\"data\":%s,\"message\":%q,\"submessage\":%q}", data, message, subMessage)
+	fmt.Print(response)
+	return localC.JSONBlob(requestCode, []byte(response))
 }
